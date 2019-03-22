cfe507a2361d9fbef7e32ef3553cb4de44fb6aea
DNAthan42 <douglasnathan42@gmail.com>
2019-03-21 18:53:47 -0600

    Started structuring agents.

---
+++ Assets/Scripts/Agent.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Agent : MonoBehaviour
+{
+    private static int AgentSize = 9;
+    private Atom[,,] atoms;
+    private Vector3 center;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        atoms = new Atom[AgentSize, AgentSize, AgentSize];
+        center = new Vector3(4, 4, 4);
+        Atom atom = Atom.Create(center);
+        atoms[4, 4, 4] = atom;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Atom.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Atom : MonoBehaviour
+{
+    private MeshFilter meshFilter;
+    private MeshRenderer meshRenderer;
+    private BoxCollider boxCollider;
+
+    public static Atom Create(Vector3 pos)
+    {
+        GameObject gameObject = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        Atom atom = gameObject.AddComponent<Atom>();
+        atom.Create(gameObject.GetComponent<MeshFilter>(), 
+            gameObject.GetComponent<MeshRenderer>(), 
+            gameObject.GetComponent<BoxCollider>(), 
+            pos);
+
+        return atom;
+    }
+
+    private void Create(MeshFilter filter, MeshRenderer renderer, BoxCollider collider, Vector3 pos)
+    {
+        meshFilter = filter;
+        meshRenderer = renderer;
+        boxCollider = collider;
+
+        transform.localPosition = pos;
+        transform.localScale *= .5f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
