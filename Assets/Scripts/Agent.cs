375c0cc50e04e76a18fa066862fac4d674feb41c
DNAthan42 <douglasnathan42@gmail.com>
2019-04-04 16:52:31 -0600

    Fixed bug where atom array was being accessed before instantiation.

--- Assets/Scripts/Agent.cs
+++ Assets/Scripts/Agent.cs
@@ -5,16 +5,12 @@ using UnityEngine;
 public class Agent : MonoBehaviour
 {
     private static int AgentSize = 9;
-    private Atom[,,] atoms;
+    private Atom[,,] atoms = new Atom[AgentSize, AgentSize, AgentSize];
     private Vector3 center;
 
     // Start is called before the first frame update
     void Start()
     {
-
-        atoms = new Atom[AgentSize, AgentSize, AgentSize];
-
-        //sample();
 
     }
 
